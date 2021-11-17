9c54f4a4191bace39b31c3af9da95fa6d04c3d4f
anamhossain0410 <anam.hossain@multidots.com>
2021-11-17 16:35:52 +0600

    update code with phpfmt formatter

--- wp-cli-custom-command.php
+++ wp-cli-custom-command.php
@@ -13,20 +13,20 @@
  */
 
 // Your code starts here.
-class WP_CLI_Custom_Command{
+class WP_CLI_Custom_Command {
     /**
-     * say hello to the 
+     * say hello to the
      * terminal user
      *
      * @return void
      */
-    public function test(){
-        WP_CLI::line('Hello! How you doing ? Hope you are having a good day.');
+    public function test() {
+        WP_CLI::line( 'Hello! How you doing ? Hope you are having a good day.' );
     }
 
     /**
      * export post data as csv
-     * wp custom export_post_csv 
+     * wp custom export_post_csv
      * --filename=post.csv
      * --category=1,2,3
      * --category=5
@@ -34,13 +34,13 @@ class WP_CLI_Custom_Command{
      * @param [type] $assoc_args
      * @return void
      */
-    public function export_post_csv($args, $assoc_args){
+    public function export_post_csv( $args, $assoc_args ) {
         /**
          * exception
          * =========
          * if there no filename flag
          */
-        if( !array_key_exists('filename', $assoc_args) ){
+        if ( !array_key_exists( 'filename', $assoc_args ) ) {
             WP_CLI::warning( 'Please provide a file name with --filename flag to export' );
             WP_CLI::error( 'No filename !', $exit = true );
         }
@@ -49,7 +49,7 @@ class WP_CLI_Custom_Command{
          * =========
          * if there is no value for filename flag
          */
-        if( 'boolean' == gettype($assoc_args['filename']) ){
+        if ( 'boolean' == gettype( $assoc_args['filename'] ) ) {
             WP_CLI::warning( 'Please provide a file name with --filename=filename.csv flag to export' );
             WP_CLI::error( 'No filename !', $exit = true );
         }
@@ -58,7 +58,7 @@ class WP_CLI_Custom_Command{
          * =========
          * if there is no value for category flag
          */
-        if( array_key_exists('category', $assoc_args) && 'boolean' == gettype($assoc_args['category']) ){
+        if ( array_key_exists( 'category', $assoc_args ) && 'boolean' == gettype( $assoc_args['category'] ) ) {
             WP_CLI::warning( 'You need to pass category id --category=1,2,3 inside flag' );
         }
         /**
@@ -67,61 +67,61 @@ class WP_CLI_Custom_Command{
         $filename = $assoc_args['filename'];
         /**
          * pick category from category flag
-         * if there is no category 
+         * if there is no category
          * assign zero to category id
          */
-        $category_id =  array_key_exists('category', $assoc_args) ? $assoc_args['category'] : 0;
-        if(file_exists($filename)){
-            WP_CLI::warning(sprintf('file already exists ! The following file will be overwritten %s', $filename));
+        $category_id = array_key_exists( 'category', $assoc_args ) ? $assoc_args['category'] : 0;
+        if ( file_exists( $filename ) ) {
+            WP_CLI::warning( sprintf( 'file already exists ! The following file will be overwritten %s', $filename ) );
             WP_CLI::confirm( __( 'Do you want to proceed?', 'wordpress-examples' ) );
 
         }
         /**
          * fetch all posts
          */
-        $site_posts = get_posts([
+        $site_posts = get_posts( [
             'numberposts' => -1,
-            'category' => +$category_id
-        ]);
+            'category'    => +$category_id,
+        ] );
         /**
-         * array of data 
+         * array of data
          * to insert into csv file
          */
-        $results = [];
+        $results   = [];
         $results[] = ['ID', 'post_title', 'post_link', 'post_date', 'post_status'];
         /**
-         * loop through each post 
-         * that is return 
+         * loop through each post
+         * that is return
          */
-        foreach($site_posts as $post){
-            $results[] = [ $post->ID , $post->post_title, $post->guid, $post->post_date, $post->post_status ];
+        foreach ( $site_posts as $post ) {
+            $results[] = [$post->ID, $post->post_title, $post->guid, $post->post_date, $post->post_status];
         }
         /**
-         * write all data 
+         * write all data
          * to csv file
          */
         $fp = fopen( $filename, 'w+' );
-        foreach($results as $result){
-            fputcsv($fp, $result);
+        foreach ( $results as $result ) {
+            fputcsv( $fp, $result );
         }
-        fclose($fp);
+        fclose( $fp );
         /**
          * make a success notice
          */
-        WP_CLI::success( sprintf('%s file exported', $filename) );
+        WP_CLI::success( sprintf( '%s file exported', $filename ) );
     }
 }
 /**
  * register custom command
- * all functions inside 
+ * all functions inside
  * WP_CLI_Custom_Command
  * are subcommand
  * @return void
  */
-function wp_cli_register_custom_commands(){
-    WP_CLI::add_command('custom', 'WP_CLI_Custom_Command');
+function wp_cli_register_custom_commands() {
+    WP_CLI::add_command( 'custom', 'WP_CLI_Custom_Command' );
 }
 /**
  * initiate cli custom command
  */
-add_action('cli_init', 'wp_cli_register_custom_commands');
+add_action( 'cli_init', 'wp_cli_register_custom_commands' );
